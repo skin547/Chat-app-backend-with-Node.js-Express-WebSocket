7541f2bbd833a3afae828e15b0601981a012def6
skin547 <skin547yo@gmail.com>
2021-05-20 00:43:28 +0800

    implement mock item repository

--- src/framework/repository/mockItemRepository.js
+++ src/framework/repository/mockItemRepository.js
@@ -6,7 +6,7 @@ module.exports = class MockItemRepository extends ItemRepository {
     constructor(){
         console.log( "Initializing MockItemRepository...")
         super()
-        this.currentId = 0
+        this.currentId = 1
         this.items = []
     }
 
@@ -16,6 +16,41 @@ module.exports = class MockItemRepository extends ItemRepository {
             this.currentId = this.currentId + 1
             this.items.push( itemInstance )
             resolve( itemInstance )
+        })
+    }
+
+    getAll(){
+        return new Promise( ( resolve ) => {
+            resolve( this.items )
+        })
+    }
+
+    getItemById( id ){
+        return new Promise( ( resolve, reject ) => {
+            this.items.forEach( item => {
+                if( Number(item.id) === Number(id) ) resolve( item )
+            })
+            reject( { error: "item not found" } )
+        })
+    }
+
+    getItemsByStatus( status ){
+        return new Promise( ( resolve ) => {
+            let foundedItems = []
+            this.items.forEach( item => {
+                if( item.status === status ) foundedItems.push( item )
+            })
+            resolve( foundedItems )
+        })
+    }
+
+    getItemsByOwner( owner ){
+        return new Promise( ( resolve ) => {
+            let foundedItems = []
+            this.items.forEach( item => {
+                if( item.owner === owner ) foundedItems.push( item )
+            })
+            resolve( foundedItems )
         })
     }
 }
