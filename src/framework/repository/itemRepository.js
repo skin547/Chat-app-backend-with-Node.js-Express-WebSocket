c81dd1b5cb38ca352af63e5d113534dab9d7de3d
skin547 <skin547yo@gmail.com>
2021-05-20 00:42:55 +0800

    add itemRepository

--- src/framework/repository/itemRepository.js
+++ src/framework/repository/itemRepository.js
@@ -4,7 +4,19 @@ module.exports = class ItemRepository {
         throw new Error( "not implemented" )
     }
 
+    getAll(){
+        throw new Error( "not implemented" )
+    }
+
     getItemById( id ){
+        throw new Error( "not implemented" )
+    }
+
+    getItemsByStatus( status ){
+        throw new Error( "not implemented" )
+    }
+
+    getItemsByOwner( owner ){
         throw new Error( "not implemented" )
     }
 }
