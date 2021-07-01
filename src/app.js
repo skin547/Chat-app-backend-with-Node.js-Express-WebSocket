bed82263d73372f81d713ba74bd166bf5bc80b2f
skin547 <skin547yo@gmail.com>
2021-07-01 19:27:47 +0800

    add: implement websocket

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const http = require("http")
+const WebSocket = require("ws")
 
 const apiRouter = require("./framework/web/router/apiRouter")
 
@@ -11,7 +12,29 @@ app.use('/api', apiRouter)
 
 const port = 8000
 
-// module.exports = app.listen( port, () => console.log( "starting server" ) );
-const server = http.createServer( app );
+const server = http.createServer( app )
+const ws = new WebSocket.Server( {server} )
 
-module.exports = server.listen( port, () => console.log( `server running on port ${port}`) );
+let clients = {}
+let id = 1
+
+ws.on( "connection", (connection) => {
+    if( !connection.id ){
+        connection.id = id
+        clients[id] = connection
+        connection.send( `here's your id: ${id}` )
+        id++
+    }
+    connection.on("message", ( message ) => {
+        data = JSON.parse( message ).data
+        console.log( data )
+        if( clients[data.to] && clients[data.from] ){
+            clients[data.to].send( data.message )
+        }else{
+            connection.send("user not connect")
+        }
+    })
+
+})
+
+module.exports = server.listen( port, () => console.log( `server running on port ${port}`) )
