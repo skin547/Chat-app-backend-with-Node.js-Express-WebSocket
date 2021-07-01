d8f0bf103b72b96091849bd151cb461825832576
skin547 <skin547yo@gmail.com>
2021-07-01 19:07:06 +0800

    refactor: error handle for jwt verifcation

--- src/framework/web/middleware/errorHandler.js
+++ src/framework/web/middleware/errorHandler.js
@@ -1,4 +1,8 @@
 errorHandler = (error, request, response, next) => {
+    console.log( error.name )
+    if( error.name === "JsonWebTokenError" ){
+        response.status(401).json( error )
+    }
     response.status(500).json(error);
 };
 
