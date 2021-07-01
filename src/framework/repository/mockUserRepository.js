1774636b4bba1609bddc3b76e58f683514fb3480
skin547 <skin547yo@gmail.com>
2021-07-01 19:05:04 +0800

    refactor: change implementation in mockUserRepo

--- src/framework/repository/mockUserRepository.js
+++ src/framework/repository/mockUserRepository.js
@@ -7,36 +7,37 @@ module.exports = class MockUserRepository extends UserRepository {
         console.log( "Initializing MockUserRepository...")
         super()
         this.currentId = 1
-        this.users = []
+        this.users = {}
     }
 
     insert( name, email, password ){
         return new Promise( ( resolve ) => {
             let newUser = new User( this.currentId, name, email, password )
+            this.users[ this.currentId ] = newUser
             this.currentId = this.currentId + 1
-            this.users.push( newUser )
             resolve( newUser )
         })
     }
 
     getUserByEmail( email ){
-        return new Promise( ( resolve ) => {
-            this.users.forEach( user => {
+        return new Promise( ( resolve, reject ) => {
+            for (let id in this.users) {
+                let user = this.users[id]
                 if( user.email === email ){
-                    delete user.password
                     resolve( user )
                 }
-            })
+            }
+            reject( new Error( "user not found"))
         })
     }
 
     getUserById( id ){
         return new Promise( ( resolve ) => {
-            this.users.forEach( user => {
-                if( user.id === id ){
-                    resolve( user )
-                }
-            })
+            if( this.users[id] ){
+                resolve( this.users[id] )
+            }else{
+                reject(new Error( "user not found"))
+            }
         })
     }
 }
