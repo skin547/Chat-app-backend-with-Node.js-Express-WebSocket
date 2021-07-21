7b2566461a820cf095d51bb7d71c1b4de008aec0
skin547 <skin547yo@gmail.com>
2021-07-22 00:05:59 +0800

    refactor: add authentication for message api, add dependency for message controller

--- src/framework/web/router/messageRouter.js
+++ src/framework/web/router/messageRouter.js
@@ -2,12 +2,18 @@ const express = require("express")
 
 const MessageController = require("../../../controller/messageController")
 const MockMessageRepository = require("../../repository/message/mockMessageRepository")
+const MockRoomRepository = require("../../repository/room/mockRoomRepository")
+const MockClientRepository = require("../../repository/client/mockClientRepository")
+const Authentication = require("../middleware/authentication.js")
 
 messageRouter = () => {
     const router = express.Router()
+    router.use( Authentication )
 
     let messageRepo = MockMessageRepository
-    let messageController = MessageController( messageRepo )
+    let roomRepo = MockRoomRepository
+    let clientRepo = MockClientRepository
+    let messageController = MessageController( messageRepo, roomRepo, clientRepo )
     router.get("/", messageController.getAll )
     router.post("/", messageController.createMessage )
     // router.post("/", messageController )
