b59331c5240de9e5cabff44187610dd0cbdc6761
skin547 <skin547yo@gmail.com>
2021-07-21 23:04:19 +0800

    add: authentication middleware

---
+++ src/framework/web/middleware/authentication.js
@@ -0,0 +1,16 @@
+const jwt = require("../../utilities/jwt")
+const secretKey = require("../../../../config").authentication.secretkey
+
+module.exports = ( request, response, next ) => {
+    try {
+        const authorizationHeader = request.headers.authorization
+        if( !authorizationHeader ) throw new Error( "Not giving authorizaiton header" )
+        const token = authorizationHeader.replace('Bearer ', '')
+        const decoded = jwt.verify( token, secretKey )
+        request.token = decoded
+        next()
+    } catch ( error ){
+        console.error( error )
+        response.status(401).json( { error : error.message} )
+    }
+}
