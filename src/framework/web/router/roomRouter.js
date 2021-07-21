5f23d538c6c00729e82ddca204acb55afebdc223
skin547 <skin547yo@gmail.com>
2021-07-22 00:06:18 +0800

    refactor: add authentication for room api

--- src/framework/web/router/roomRouter.js
+++ src/framework/web/router/roomRouter.js
@@ -3,10 +3,12 @@ const express = require("express")
 const RoomController = require("../../../controller/roomController")
 const MockRoomRepo = require("../../repository/room/mockRoomRepository")
 const MockMessageRepo = require("../../repository/message/mockMessageRepository")
+const Authentication = require("../middleware/authentication.js")
 
 roomRouter = () => {
     const router = express.Router()
 
+    router.use( Authentication )
     const roomController = RoomController( MockRoomRepo, MockMessageRepo )
     router.get("/", roomController.getAll )
     router.get("/:roomId", roomController.getById )
