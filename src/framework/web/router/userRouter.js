a589d503dd06e02c51930cbe70c8294d44ce0f7f
skin547 <skin547yo@gmail.com>
2021-07-28 16:20:22 +0800

    add: implementation for mysql repository in production

--- src/framework/web/router/userRouter.js
+++ src/framework/web/router/userRouter.js
@@ -3,11 +3,16 @@ const express = require("express")
 const UserController = require("../../../controller/userController")
 const mockRoomRepository = require("../../repository/room/mockRoomRepository")
 const mockUserRepository = require("../../repository/user/mockUserRepository")
+const mysqlUserRepository = require("../../repository/user/mysqlUserRepository")
 
 userRouter = () => {
     const router = express.Router()
-
-    const userRepo = mockUserRepository
+    let userRepo
+    if( process.env.NODE_ENV === 'production'){
+        userRepo = mysqlUserRepository
+    } else {
+        userRepo = mockUserRepository
+    }
     const roomRepo = mockRoomRepository
     let userController = UserController( userRepo, roomRepo )
 
