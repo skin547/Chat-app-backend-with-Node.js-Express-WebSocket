35532f55d7ae2ffb08b360d0899815203b62d77e
skin547 <skin547yo@gmail.com>
2021-08-24 22:41:24 +0800

    update : room router with mysql repository in production

--- src/framework/web/router/roomRouter.js
+++ src/framework/web/router/roomRouter.js
@@ -3,13 +3,25 @@ const express = require("express")
 const RoomController = require("../../../controller/roomController")
 const MockRoomRepo = require("../../repository/room/mockRoomRepository")
 const MockMessageRepo = require("../../repository/message/mockMessageRepository")
+const MysqlRoomRepository = require("../../repository/room/mysqlRoomRepository.js")
 const Authentication = require("../middleware/authentication.js")
+const mysqlMessageRepository = require("../../repository/message/mysqlMessageRepository")
 
 roomRouter = () => {
     const router = express.Router()
 
+    let roomRepo
+    let messageRepo
+    if( process.env.NODE_ENV === 'production'){
+        roomRepo = MysqlRoomRepository
+        messageRepo = mysqlMessageRepository
+    } else {
+        roomRepo = MockRoomRepo
+        messageRepo = MockMessageRepo
+    }
+    
     router.use( Authentication )
-    const roomController = RoomController( MockRoomRepo, MockMessageRepo )
+    const roomController = RoomController( roomRepo, messageRepo )
     router.get("/", roomController.getAll )
     router.get("/:roomId", roomController.getById )
     router.get("/:roomId/messages", roomController.getRoomMessages )
