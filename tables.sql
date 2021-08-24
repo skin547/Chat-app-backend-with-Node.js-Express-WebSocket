8d2eae4100bf32060085eac967af12b54e0d7b63
skin547 <skin547yo@gmail.com>
2021-08-24 22:10:35 +0800

    add : create messages table

--- tables.sql
+++ tables.sql
@@ -1,4 +1,4 @@
 create table users (id char(36) PRIMARY KEY, name varchar(50), email varchar(64), password varchar(64), createdAt date);
 create table rooms (id char(36) PRIMARY Key, name varchar(50), createdAt date);
 
-create table room_user ( roomId char(36), userId char(36), FOREIGN Key(roomId) REFERENCES rooms(id) ON DELETE CASCADE , FOREIGN Key(userId) REFERENCES users(id) ON DELETE CASCADE );
+create table room_user ( roomId char(36), userId char(36), FOREIGN Key(roomId) REFERENCES rooms(id) ON DELETE CASCADE , FOREIGN Key(userId) REFERENCES users(id) ON DELETE CASCADE );create table messages ( id char(36), content TEXT, userId char(36), roomId char(36), createdAt date, updatedAt date, FOREIGN Key(roomId) REFERENCES rooms(id) ON DELETE CASCADE , FOREIGN Key(userId) REFERENCES users(id) ON DELETE SET NULL)
