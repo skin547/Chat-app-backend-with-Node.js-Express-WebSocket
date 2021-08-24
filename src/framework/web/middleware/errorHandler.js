f1cce75e6d1be69f24b6b6509e90e4f8b0ab0081
skin547 <skin547yo@gmail.com>
2021-08-24 22:37:57 +0800

    fix : error handle for jsonwebtoken with correspond status code

--- src/framework/web/middleware/errorHandler.js
+++ src/framework/web/middleware/errorHandler.js
@@ -4,11 +4,10 @@ errorHandler = (error, request, response, next) => {
     logger.log( {level:'error', message: error} )
     if( error.name === "JsonWebTokenError" ){
         switch( error.message ){
-            case "invalid token" :
-                response.status( 401 ).json( { error : "invalid token" })
-                break;
             case "invalid signature" :
-                response.status( 400 ).json( { error : "invalid signature" } )
+                return response.status( 400 ).json( { error : "ivalid signnature" } )
+            case "invalid token" :
+                return response.status( 401 ).json( { error : "invalid token" })
         }
         
     }
